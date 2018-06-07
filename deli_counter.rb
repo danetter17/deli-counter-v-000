2b2ca6b423083751974e4bf5e8c374cbf18281d0
Daniel Etter <etter.dt@gmail.com>
2018-06-07 20:46:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+require "pry"
+katz_deli=[]
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    current_line = []
+    katz_deli.each_with_index do |name, index|
+    #binding.pry
+    current_line << "#{index+1}. #{name}"
+    end
+    puts "The line is currently: #{current_line.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, customer)
+  current_line = []
+  if katz_deli == 0
+    
+    
+end
+
+def now_serving
+end
